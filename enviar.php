287f95054876bd0dee82da55a424797bdaf16400
Lkplmz <lkplmz@gmail.com>
2024-11-10 21:03:50 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST METHOD"] == "POST") {
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $email = htmlspecialchars($_POST['email']);
+
+    $to = 'lkplmz@gmail.com'; 
+    $subject = 'Nuevo Mensaje de Contacto de la Pagina Web'
+    $message = "Nombre: $nombre\nCorreo: $email";
+    $headers = "From: $email" .
+    "\r\n" . 
+    "Reply-To: $email" . 
+    "\r\n" . 
+    "X-Mailer: PHP/" . 
+    phpversion();
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Mensaje enviado con exito.";
+    } else {
+        echo "Error al enviar mensaje.";
+        }
+    } else {
+        echo "Metodo no permitido.";
+    }
+
+?>
